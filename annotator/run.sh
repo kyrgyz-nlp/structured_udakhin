cdbafddadbf203399c0df64d87488e334860bba4
murat <jumasheff@gmail.com>
2024-07-24 21:43:14 +0600

    Make it possible to run prodigy locally and on cloud

--- annotator/run.sh
+++ annotator/run.sh
@@ -3,10 +3,10 @@ python scripts/mkconfig.py
 
 # Small bit of code that downloads data, can be replaced with proper script
 # that fetches data from s3 or another storage source.
-wget https://raw.githubusercontent.com/explosion/prodigy-recipes/master/example-datasets/news_headlines.jsonl
+# wget https://raw.githubusercontent.com/explosion/prodigy-recipes/master/example-datasets/news_headlines.jsonl
 
 # Show the stats, just for the logs
 python -m prodigy stats
 
 # Start Prodigy
-python -m prodigy ner.manual nyt blank:en news_headlines.jsonl --label "COMPANY,PERSON"
+python -m prodigy spans.manual yudakhin_001 blank:ky dataset_for_annotation_5000.jsonl --label "Ашыкча берилген,Ордунда эмес,Жетишпейт"
---
+++ annotator/run_locally.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Ensure the script exits if any command fails
+set -e
+
+# Enable Docker BuildKit
+export DOCKER_BUILDKIT=1
+
+# Define the .env.local file
+ENV_FILE=".env.local"
+
+# Function to write secrets to individual files
+write_secret() {
+  local key=$1
+  local value=$2
+  echo -n "$value" > "secrets/$key"
+}
+
+# Read .env.local file and write each secret to its own file
+mkdir -p secrets
+while IFS='=' read -r key value; do
+  # Skip comments and empty lines
+  [[ "$key" =~ ^#.*$ ]] && continue
+  [[ -z "$key" ]] && continue
+
+  # Write the secret to its file
+  write_secret "$key" "$value"
+done < $ENV_FILE
+
+# Run docker-compose
+docker compose up -d
+
+# Remove the secrets folder
+# rm -rf secrets
+
+echo "Docker containers are up and secrets folder has been removed."
